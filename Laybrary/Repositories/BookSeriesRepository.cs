af3b0759926ea48f9625c256f1c549579b3c40b9
dsestari <43218907+dsestari@users.noreply.github.com>
2020-06-24 13:39:05 +0100

    Update BookSeriesRepository.cs

--- Laybrary/Repositories/BookSeriesRepository.cs
+++ Laybrary/Repositories/BookSeriesRepository.cs
@@ -1,12 +1,43 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Laybrary.Models;
+using System.Windows.Forms;
 
 namespace Laybrary.Repositories
 {
     public class BookSeriesRepository
     {
+        private int GetNextOrder()
+        {
+            using (Context db = new Context())
+            {
+                var lastOrder = db.BookSeries.OrderByDescending(bs => bs.Registration_Order).Select(b => b.Registration_Order).First();
+
+                if (lastOrder != null)
+                {
+                    return (int)lastOrder + 1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+        public int SuggestNextOrder()
+        {
+            try
+            {
+                return GetNextOrder();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error when trying to get next serie order :" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 0;
+            }
+        }
     }
 }
