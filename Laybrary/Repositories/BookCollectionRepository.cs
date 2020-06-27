35355dfaac643f3f23125ebde0e314991fe9a063
dsestari <43218907+dsestari@users.noreply.github.com>
2020-06-27 14:49:11 +0100

    Add files via upload
    add new class

---
+++ Laybrary/Repositories/BookCollectionRepository.cs
@@ -0,0 +1,27 @@
+﻿using Laybrary.Models;
+using Laybrary.Useful;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laybrary.Repositories
+{
+    public class BookCollectionRepository
+    {
+        private List<BookCollection> GetCollection()
+        {
+            using (Context db = new Context())
+            {
+                return db.BookCollections.OrderBy(c => c.Registration_Order).ToList();
+            }
+        }
+
+        public DataTable LoadCollectionOnDataGrid()
+        {
+            return Helper.ToDataTable(GetCollection());
+        }
+    }
+}
