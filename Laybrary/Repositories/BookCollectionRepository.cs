2d46ac3e800ff209b587928b9b6086828fe2e88f
dsestari <43218907+dsestari@users.noreply.github.com>
2020-06-28 19:39:05 +0100

    Update BookCollectionRepository.cs
    working on BookCollectionRepository

--- Laybrary/Repositories/BookCollectionRepository.cs
+++ Laybrary/Repositories/BookCollectionRepository.cs
@@ -1,8 +1,9 @@
-﻿using Laybrary.Models;
+using Laybrary.Models;
 using Laybrary.Useful;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +19,26 @@ namespace Laybrary.Repositories
                 return db.BookCollections.OrderBy(c => c.Registration_Order).ToList();
             }
         }
+
+        private List<BookCollection> SearchCollection(string _name, string _author, int _numOfPages, string _publisher, decimal _amount, string _local, DateTime _purchaseDate)
+        {
+            using (Context db = new Context())
+            {                
+                var result = db.BookCollections;
+
+                if (!String.IsNullOrEmpty(_name))
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Name.Contains(_name));
+                }
+
+                if (!String.IsNullOrEmpty(_author))
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Author.Contains(_author));
+                }
+
+                return result.ToList();
+            }
+        } 
 
         public DataTable LoadCollectionOnDataGrid()
         {
