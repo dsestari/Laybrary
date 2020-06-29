1443bf8a24cad0712a935a9354b1163ab4bbd5e1
dsestari <43218907+dsestari@users.noreply.github.com>
2020-06-29 16:50:09 +0100

    Update BookCollectionRepository.cs
    updating class

--- Laybrary/Repositories/BookCollectionRepository.cs
+++ Laybrary/Repositories/BookCollectionRepository.cs
@@ -7,6 +7,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Laybrary.Repositories
 {
@@ -20,7 +21,7 @@ namespace Laybrary.Repositories
             }
         }
 
-        private List<BookCollection> SearchCollection(string _name, string _author, int _numOfPages, string _publisher, decimal _amount, string _local, DateTime _purchaseDate)
+        private List<BookCollection> SearchCollection(string _name, string _author, int _numOfPages, string _publisher, bool _searchAmount, decimal _amount, string _local, DateTime _purchaseDate)
         {
             using (Context db = new Context())
             {                
@@ -36,13 +37,151 @@ namespace Laybrary.Repositories
                     result = (DbSet<BookCollection>)result.Where(c => c.Author.Contains(_author));
                 }
 
-                return result.ToList();
+                if (_numOfPages != 0)
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Number_of_Pages == _numOfPages);
+                } 
+
+                if (!String.IsNullOrEmpty(_publisher))
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Publisher.Contains(_publisher));
+                }
+
+                if (_searchAmount == true)
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Amount == _amount);
+                }
+
+                if (!String.IsNullOrEmpty(_local))
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Local.Contains(_local));
+                }
+
+                if (_purchaseDate != null)
+                {
+                    result = (DbSet<BookCollection>)result.Where(c => c.Purchase_Date == _purchaseDate);
+                }
+                
+                return result.OrderBy(c => c.Registration_Order).ToList();
             }
-        } 
+        }
+
+        private int GetNextOrder()
+        {
+            using (Context db = new Context())
+            {
+                var lastOrder = db.BookCollections.OrderByDescending(c => c.Registration_Order).Select(c => c.Registration_Order).First();
+
+                if (lastOrder != null)
+                {
+                    return (int)lastOrder + 1;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+        }
+
+        private int GetCollectionId(string _name, string _author, string _publisher)
+        {
+            using (Context db = new Context())
+            {
+                return db.BookCollections.Single(c => c.Name == _name && c.Author == _author && c.Publisher == _publisher).Id;
+            }
+        }        
+
+        private void UpdateCollection(BookCollection bookCollection)
+        {
+            using (Context db = new Context())
+            {
+                if (bookCollection.Id == 0)
+                {
+                    db.BookCollections.Add(bookCollection);
+                }
+                else
+                {
+                    var model = db.BookCollections.Single(c => c.Id == bookCollection.Id);
+
+                    if (model != null)
+                    {
+                        model.Name = bookCollection.Name;
+                        model.Author = bookCollection.Author;
+                        model.Number_of_Pages = bookCollection.Number_of_Pages;
+                        model.Publisher = bookCollection.Publisher;
+                        model.Amount = bookCollection.Amount;
+                        model.Local = bookCollection.Local;
+                        model.Purchase_Date = model.Purchase_Date;                        
+                    }
+                }
+
+                db.SaveChanges();
+            }
+        }
 
         public DataTable LoadCollectionOnDataGrid()
         {
             return Helper.ToDataTable(GetCollection());
+        }
+
+        public DataTable SearchCollectionValidation(string name, string author, int numOfPages, string publisher, bool searchAmount, decimal amount, string local, DateTime purchaseDate)
+        {
+            return Helper.ToDataTable(SearchCollection(name, author, numOfPages, publisher, searchAmount, amount, local, purchaseDate));
+        }
+
+        public int SuggestNextOrder()
+        {
+            try
+            {
+                return GetNextOrder();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error when trying to get next collection order :" + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 1;
+            }
+        }
+
+        public int GetCollectionIdValidation(string name, string author, string publisher)
+        {
+            if (!String.IsNullOrEmpty(name) && !String.IsNullOrEmpty(author) && !String.IsNullOrEmpty(publisher))
+            {
+                try
+                {
+                    return GetCollectionId(name, author, publisher);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error when trying to get Collection Id, details: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("The name, author and publisher are required to identify the Collection.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return 0;
+            }
+        }
+
+        public void UpdateCollectionValidation(BookCollection bookCollection)
+        {
+            if (bookCollection.Registration_Order > 0 && !String.IsNullOrEmpty(bookCollection.Name) && !String.IsNullOrEmpty(bookCollection.Author) && !String.IsNullOrEmpty(bookCollection.Publisher))
+            {
+                int collectionId = GetCollectionId(bookCollection.Name, bookCollection.Author, bookCollection.Publisher);
+
+                if (collectionId != 0)
+                {
+                    bookCollection.Id = collectionId;
+                }
+                else
+                {
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("The Collection Name, Author and Publisher are required and Order should be granter then 0.", "Attention", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
